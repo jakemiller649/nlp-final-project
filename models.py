131081e0e435bf25ec778e82d30bb09625f58205
jakemiller649 <carrolljmiller@gmail.com>
2019-03-31 19:00:39 -0400

    LSTM (khanpour) seems to work

--- models.py
+++ models.py
@@ -1,4 +1,10 @@
-"""description goes here"""
+"""description goes here
+
+TO DO:
+- add dropout to CNN
+- parent class?
+
+"""
 
 import numpy as np
 from keras.models import Model
@@ -14,14 +20,59 @@ def embedding_layer(corpus, embed_dim, batch_size, inputs):
                        trainable = True,
                        name = "embedding")(inputs)
 
-class BiLSTMSoftmax:
-    # eventually change name
-    """model pased on Khanpour et al """
+class LSTMSoftmax:
+    """This modle is based on Khanpour et al 2016. Paper://www.aclweb.org/anthology/C16-1189
+    """
 
-    def __init__(self, corpus, base, output, **params):
-        """ xxx """
 
-        pass
+    def __init__(self, corpus, batch_size = 25, num_layers = 2,
+                dropout_rate = 0.5, hidden_state_size = 100, stateful = False):
+        """
+
+        Note to self, it does not sound like they passed states between batches, but why can't I try?
+        """
+
+        # this model only takes one utterance at a time, so its input shape is
+        # [batch_size, utterance_length]
+        inputs_ = Input(shape = (corpus.max_utt_length,), name = "input")
+
+        # embedding layer
+        embed_dim = corpus.embed_dim
+        x_ = embedding_layer(corpus = corpus,
+                             batch_size = batch_size, embed_dim = embed_dim,
+                             inputs = inputs_)
+        # shape is now [batch_size, utterance_length, embed_dim]
+
+        # dropout layer
+        x_ = Dropout(rate = dropout_rate, name = "dropout")(x_)
+
+        # LSTM layers
+        for i in range(num_layers):
+            if i == 0:
+                # first layer comes from embedding
+                h_in_ = x_
+            else:
+                # subsequent inputs come from previous layers
+                h_in_ = h_out_
+
+            h_out_ = LSTM(units = hidden_state_size,
+                          return_sequences = True, stateful = stateful,
+                          name = "lstm_" + str(i))(h_in_)
+
+            # output shape here is [batch_size, utterance_length, hidden_state_size]
+
+        # pooling layer
+        # they do "last pooling" -- ie, they just take the last output from the lstm layer
+        lstm_out_ = Lambda(lambda x: x[:,-1,:], name = "last_pooling")(h_out_)
+        # shape is now [batch_size, hidden_state_size]
+
+        # output layer
+        predictions_ = Dense(16, activation='softmax', name = 'softmax_output')(lstm_out_)
+
+        self.model = Model(inputs = inputs_, outputs=predictions_)
+
+    def compile(self):
+        self.model.compile(optimizer = 'adagrad', metrics = ['acc'], loss = 'categorical_crossentropy')
 
 
 class CNN:
@@ -39,10 +90,9 @@ class CNN:
 
     def __init__(self, corpus, batch_size, conv_params, hidden_units, d1 = 0, d2 = 0):
         """
+        do I even need build_model??? cannot I not condense these into one function?
+        what about compile??
         """
-
-        ### need some kind of multi-utterance generator
-        ## gives me three at a time
 
         self.corpus = corpus
         self.seq_length = d1 + d2 + 1
@@ -113,7 +163,7 @@ class CNN:
             ff_t_ = Concatenate(axis = -1)(ff_outputs)
         # shape is now [batch_size, (d2+1) * hidden_units]
 
-        # ANOTHER DROPOUT??
+        # ANOTHER DROPOUT?? -- they did not do that
 
         # output layer
         predictions_ = Dense(16, activation='softmax', name = 'softmax_output')(ff_t_)
