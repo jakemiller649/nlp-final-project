40fbcf971e54fda329ee6ae28e91b298a0449e53
jakemiller649 <carrolljmiller@gmail.com>
2019-04-03 23:02:14 +0000

    fixed some bugs in generator

--- utils.py
+++ utils.py
@@ -438,26 +438,29 @@ class UtteranceGenerator(Sequence):
         # Returns
             A batch
         """
-        start = idx * self.batch_size
-        end = (idx + 1) * self.batch_size
+        start = idx * self.batch_size #+ self.sequence_length - 1
+        end = (idx + 1) * self.batch_size #+ self.sequence_length - 1
+        
+        if end > len(self.combined_utterances) - self.sequence_length: # end cases
+            end = len(self.combined_utterances) - self.sequence_length
 
         if self.sequence_length == 1:
             batch_x = np.array([u.word_ids for u in self.combined_utterances[start:end]])
         else:
             sub_batches_x = []
             for idx in range(start,end):
-                sub_batch_x = [u.word_ids for u in self.combined_utterances[(idx - self.sequence_length + 1):idx+1]]
+                sub_batch_x = np.array([u.word_ids for u in self.combined_utterances[idx:idx+self.sequence_length]])
                 sub_batches_x.append(sub_batch_x)
 
             batch_x = np.array(sub_batches_x)
             # batch x shape is [batch_size, sequence_length, utterance_length]
 
-        batch_y = np.array([u.da_type for u in self.combined_utterances[start:end]])
+        batch_y = np.array([u.da_type for u in self.combined_utterances[start:end]], dtype = np.int)
         # batch_y shape is currently [batch,]
         
         # convert batch_y to one-hot
-        batch_y_oh = np.zeros((self.batch_size, 17))
-        batch_y_oh[np.arange(self.batch_size), batch_y] = 1
+        batch_y_oh = np.zeros((batch_y.shape[0], 17))
+        batch_y_oh[np.arange(batch_y.shape[0]), batch_y] = 1
         
         # batch_y_oh is [batch_size, 17]
         
@@ -468,10 +471,9 @@ class UtteranceGenerator(Sequence):
         # Returns
             The number of batches in the Sequence.
         """
-        return int(np.ceil(len(self.combined_utterances) / float(self.batch_size)))
+        return int(np.ceil( (len(self.combined_utterances) - self.sequence_length + 1) / self.batch_size) )
 
     def __iter__(self):
         """Create a generator that iterate over the Sequence."""
-        for item in (self[i] for i in range(self.sequence_length - 1, len(self))):
-            # so if sequence length is 1, you start at idx 0. if it's 5 you start at idx 4
+        for item in (self[i] for i in range(len(self))):
             yield item
